f58b78125daca166111b0c902df583f3d4d1a748
Steve Kohlmeyer <Fastmoverr@gmail.com>
2014-08-17 16:04:17 -0500

    Fully working

--- sk-plugins-last-updated-column.php
+++ sk-plugins-last-updated-column.php
@@ -21,12 +21,51 @@ class SK_Plugins_Last_Updated_Column {
 
     function columnData( $column_name, $plugin_file, $plugin_data ) {
 
-        if ( 'sk_plugin_last_updated' == $column_name && 'My Plugin Name' == $plugin_data['Name'] ) {
+        if ( 'sk_plugin_last_updated' == $column_name ) {
 
-            echo 'My Plugin custom column data';
+            $pluginDirectory = explode('/', $plugin_file);
+            $lastUpdated = $this->getPluginsLastUpdated($pluginDirectory[0]);
+            echo $lastUpdated;
 
         }
 
+    }
+
+    function getPluginsLastUpdated($pluginSlug) {
+
+        if(!get_transient('sk_plugins_last_updated' . $pluginSlug)) {
+
+            include_once(ABSPATH . 'wp-admin/includes/plugin-install.php');
+
+            $call_api = plugins_api( 'plugin_information', array( 'slug' => $pluginSlug, 'fields' => array('last_updated') ) );
+
+            /** Check for Errors & Display the results */
+            if ( is_wp_error( $call_api ) ) {
+
+                set_transient('sk_plugins_last_updated' . $pluginSlug, 'Not Availble', 86400);
+                return "Not Available";
+
+            } else {
+
+                if ( ! empty( $call_api->last_updated ) ) {
+
+                    set_transient('sk_plugins_last_updated' . $pluginSlug, $call_api->last_updated, 86400);
+                    return $call_api->last_updated;
+
+                } else {
+
+                    set_transient('sk_plugins_last_updated' . $pluginSlug, 'Not Availble', 86400);
+                    return "Not Available";
+
+                }
+
+            }
+
+        } else {
+
+            return get_transient('sk_plugins_last_updated' . $pluginSlug);
+
+        }
     }
 
     function columnHeading( $columns ) {
@@ -38,6 +77,6 @@ class SK_Plugins_Last_Updated_Column {
 
 }
 
-
+$SK_Plugins_Last_Updated_Column = new SK_Plugins_Last_Updated_Column();
 
 ?>
